4daec4a5038040d3ef2225016bfbf673b2cd05b6
Xavier Soler Delgado <xavier@MacBook-MacBook-Pro-de-Xavier.local>
2020-04-24 20:02:54 +0200

    plugins

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
-//const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
     mode: 'development',
@@ -15,10 +17,11 @@ module.exports = {
             }
         ]
     },
-    /* plugins: [
+     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]*/
+        }),
+        //new BundleAnalyzerPlugin()
+    ]
 }
